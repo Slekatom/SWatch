e723742cb77158b3d5017c316f4b2437591a195c
Slekatom <nemo.bma@gmail.com>
2025-03-23 23:07:28 +0200

    Models added

--- SWatch/full_videos/models.py
+++ SWatch/full_videos/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Channel(models.Model):
+    COUNTRIES = [
+        ("ua", "Ukraine"),
+        ("us", "USA"),
+        ("ge", "Germany"),
+        ("fr", "France"),
+        ("uk", "United Kingdom"),
+        ("404", "Non defined")
+    ]
+
+    title = models.CharField(max_length=20)
+    description = models.TextField(max_length=100)
+    followers = models.PositiveIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
+    country = models.CharField(max_length=20, choices=COUNTRIES, default="404")
+
+    def __str__(self):
+        return f"Channel: {self.title}, Country: {self.country}"
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=200)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    views = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"Video: {self.title}, Channel: {self.channel.title}"
